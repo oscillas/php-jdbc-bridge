51c12067661a15aae853941bcb28adbdb6304758
Jacob Ray Gilman <jacob@oscillas.com>
2023-09-06 16:45:59 -0500

    Don't download Commons Daemon again if it's already in the lib folder

--- java/build.sh
+++ java/build.sh
@@ -4,13 +4,18 @@ mkdir -p lib class tmp
 
 COMMONS_DAEMON_VER=commons-daemon-1.2.2
 
-wget \
-  -O tmp/${COMMONS_DAEMON_VER}-bin.tar.gz \
-  "https://archive.apache.org/dist/commons/daemon/binaries/${COMMONS_DAEMON_VER}-bin.tar.gz"
-tar -C lib/ \
-  --strip-components=1 \
-  -zxvf tmp/${COMMONS_DAEMON_VER}-bin.tar.gz \
-  ${COMMONS_DAEMON_VER}/${COMMONS_DAEMON_VER}.jar
+if [ ! -f lib/${COMMONS_DAEMON_VER}.jar ]
+then
+  wget \
+    -O tmp/${COMMONS_DAEMON_VER}-bin.tar.gz \
+    "https://archive.apache.org/dist/commons/daemon/binaries/${COMMONS_DAEMON_VER}-bin.tar.gz"
+  tar -C lib/ \
+    --strip-components=1 \
+    -zxvf tmp/${COMMONS_DAEMON_VER}-bin.tar.gz \
+    ${COMMONS_DAEMON_VER}/${COMMONS_DAEMON_VER}.jar
+else
+  echo "Commons Daemon already up to date."
+fi
 
 javac -cp lib/${COMMONS_DAEMON_VER}.jar -d class/ src/*
 jar cfe lib/pjbridge.jar Server -C class .
